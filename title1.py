279db914bd387f94755d2400a089b4b269191beb
sarahgupta2012-ops <sarahgupta2012@gmail.com>
2025-08-26 17:23:06 +0530

    Update title1.py

--- title1.py
+++ title1.py
@@ -1,2 +1,22 @@
 import streamlit as st
-st.title("Welcome to streamlit world!")
+
+#main title
+st.title("This is the Main title")
+#section heading
+st.header("This is the section heading")
+#sub heading
+st.subheader("This is the subheading")
+
+#Normal paragraph
+st.write("This is my normal para. It's great for description")
+
+st.markdown(
+    """
+    **I am the bold text**, *I am in Italic*,
+    and a [link to Google](https://www.google.com/)
+    """,
+    unsafe_allow_html=True #permission to use html tages in markdown
+)
+
+#caption
+st.caption("We created this with streamlit")
